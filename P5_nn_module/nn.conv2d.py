251e59618fa104d32cfe02ca22fe31a4a9df7ec9
YSH111-A <1803822866@qq.com>
2025-01-18 22:28:13 +0800

    third commit

--- P5_nn_module/nn.conv2d.py
+++ P5_nn_module/nn.conv2d.py
@@ -35,5 +35,6 @@ for data in dataloader:
     output = torch.reshape(output,(-1,3,30,30))
     writer.add_images('output',output,step)
     step +=1
-    ####fsa
+writer.close()
+
 
---
+++ P5_nn_module/nn.relu.py
@@ -0,0 +1,42 @@
+import torch
+import torchvision
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torch.nn import ReLU,Sigmoid
+
+# input = torch.tensor([
+#     [1,-0.5],
+#     [-1,3]
+# ])
+
+# input = torch.reshape(input, (-1,1,2,2))
+# print(input)
+# print(input.shape)
+
+dataset = torchvision.datasets.CIFAR10("../data_set", train=False, download=False,
+                                       transform=torchvision.transforms.ToTensor())
+dataloader = DataLoader(dataset, batch_size=64, shuffle=False)
+
+class relu1(torch.nn.Module):
+    def __init__(self):
+        super(relu1, self).__init__()
+        self.relu_1 = torch.nn.ReLU()
+        self.sigmoid_1 = torch.nn.Sigmoid()
+
+    def forward(self, input):
+        output = self.sigmoid_1(input)
+        return output
+
+shuanghe = relu1()
+
+writer = SummaryWriter("../logs_relu")
+step = 0
+for data in dataloader:
+    imgs, labels = data
+    writer.add_images("sigmod_input", imgs, global_step=step)
+    output  = shuanghe(imgs)
+    writer.add_images("sigmod_output", output, global_step=step)
+    step += 1
+
+
+writer.close()
